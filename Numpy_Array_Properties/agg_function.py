8e4d3982ec37406d8021839bc9c327167b45e583
aryan24-cs <aryan.nda.2163@gmail.com>
2025-10-21 08:35:22 +0530

    Create agg_function.py

---
+++ Numpy_Array_Properties/agg_function.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr = np.array([10 ,20 ,30 ,40 ,50])
+
+# Aggregate functions
+
+print(np.sum(arr))      # Output: 150
+print(np.mean(arr))     # Output: 30.0 
+print(np.min(arr))      # Output: 10
+print(np.max(arr))      # Output: 50
+print(np.std(arr))      # Output: 14.142135623730951
+print(np.var(arr))      # Output: 200.0
+print(np.median(arr))   # Output: 30.0
