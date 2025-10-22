bba42e7697efc7fa1843d952c35f58032a60ad5b
aryan24-cs <aryan.nda.2163@gmail.com>
2025-10-22 20:37:19 +0530

    update

---
+++ Reshaping_&_Manipulating/flatten.py
@@ -0,0 +1,10 @@
+'''
+.ravel() -> return view
+.flatten() -> return copy
+'''
+
+import numpy as np 
+
+arr = np.array([[1,2,3],[4,5,6]])
+print(arr.ravel())  # return view of original array
+print(arr.flatten())  # return copy of original array
---
+++ Reshaping_&_Manipulating/reshape.py
@@ -0,0 +1,13 @@
+"""
+reshape(rows , cols) specify new shape for an array without changing its data.
+if dimensions do not match, it will throw an error.
+reshaping doesnot crete copy , it return view of original array.
+"""
+
+import numpy as np 
+
+arr = np.array([1,2,3,4,5,6])
+print(arr)
+
+reshape_arr = arr.reshape(2,3)
+print(reshape_arr)
